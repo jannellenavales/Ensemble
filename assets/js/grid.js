43d87e00cc407e743d8d3afb6e6a37716ccf4935
jannellenavales <jannellenavales@gmail.com>
2023-05-02 16:28:52 -0500

    First Upload
    This is *hopefully* the "final" project!

---
+++ assets/js/grid.js
@@ -0,0 +1,58 @@
+
+  //below are the hover functions
+
+  let ele1 = document.querySelector("#ele1");
+  ele1.addEventListener("mouseover", function () {
+    this.src = "assets/img/Album1.jpg";
+  });
+  ele1.addEventListener("mouseout", function () {
+    this.src = "assets/img/dummyimage.png";
+  });
+
+let ele2 = document.querySelector("#ele2");
+ele2.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album2.jpeg";
+});
+ele2.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
+
+let ele3 = document.querySelector("#ele3");
+ele3.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album3.jpeg";
+});
+ele3.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
+
+let ele4 = document.querySelector("#ele4");
+ele4.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album4.jpeg";
+});
+ele4.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
+
+let ele7 = document.querySelector("#ele7");
+ele7.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album7.jpg";
+});
+ele7.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
+
+let ele8 = document.querySelector("#ele8");
+ele8.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album8.jpeg";
+});
+ele8.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
+
+let ele9 = document.querySelector("#ele9");
+ele9.addEventListener("mouseover", function () {
+  this.src = "assets/img/Album9.jpeg";
+});
+ele9.addEventListener("mouseout", function () {
+  this.src = "assets/img/dummyimage.png";
+});
